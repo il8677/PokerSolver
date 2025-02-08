d12c9434d01e323f4576003fbd8f664ec8c92036
il8677 <jiajiaisaac@gmail.com>
2025-02-08 22:37:59 +0100

    Child count

--- Solver/src/TreeNode.hpp
+++ Solver/src/TreeNode.hpp
@@ -1,7 +1,11 @@
-
+#include <vector>
+#include <memory>
 
 class TreeNode {
-
+public:
+	size_t getChildCount() const { return children_.size(); }
+private:
+	std::vector<std::unique_ptr<TreeNode>> children_;
 };
 
 class CardNode : public TreeNode {
--- Solver/tests/TreeTests.cpp
+++ Solver/tests/TreeTests.cpp
@@ -1,2 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_approx.hpp>
+#include <TreeNode.hpp>
+
+SCENARIO("Tree children management") {
+	GIVEN("A tree node") {
+		TreeNode node;
+		THEN("It starts with no children") {
+			REQUIRE(node.getChildCount() == 0);
+		}
+	}
+}
