5b7414b8433d974aeb678e9abe196009264e374d
il8677 <jiajiaisaac@gmail.com>
2025-02-08 22:47:27 +0100

    Base constraint

--- Solver/src/TreeNode.hpp
+++ Solver/src/TreeNode.hpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <memory>
 
+
+
 class TreeNode {
 public:
 	TreeNode()=default;
@@ -11,7 +13,9 @@ public:
 	size_t getChildCount() const { return children_.size(); }
 
 	template <typename T, typename... Args>
-	void emplaceChild(Args&&... args) {
+	void emplaceChild(Args&&... args) 
+		requires(std::is_base_of_v<TreeNode, T>) {
+
 		children_.emplace_back(std::make_unique<T>(args...));
 	}
 
